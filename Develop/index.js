2796e67f714ffdb00e53812a026b9825d55c48fd
MinhThu <mttnguyen91@gmail.com>
2022-11-25 20:11:43 -0600

    First part of questions array for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,27 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+// This will link the page to where the readme is developed
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// This is array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Type in your Github username.',
+        validate: usernameInput => {
+            // Accept if a valid username is inputed 
+            if (usernameInput) {
+                return true;
+            // reject and ask the user to enter a valid username if they did not
+            } else {
+                console. log('Please enter a valid Github username');
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
